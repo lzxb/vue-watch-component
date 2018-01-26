1e4c9033e52310dc98e97a2c105f5438dc1fc059
lzxb <1340641314@qq.com>
2018-01-26 10:42:37 +0800

    test: 添加单元测试

--- tests/index.js
+++ tests/index.js
@@ -22,7 +22,7 @@ ava('base', async t => {
   }
   const watchOptions = {
     watch () {
-      return this.count
+      return this.globalState.count
     },
     handler () {
       watchLog.count++
@@ -35,27 +35,345 @@ ava('base', async t => {
   const vm = new Vue({
     watchComponents: [wc],
     data () {
-      return globalState
+      return { globalState }
     }
   })
   t.is(watchLog.count, 0)
   t.is(globalState.count, 0)
-  t.is(vm.count, 0)
+  t.is(vm.globalState.count, 0)
   t.is(typeof wc.unwatch, 'function')
   t.is(wc.vm, vm)
 
   globalState.count++
   await vm.$nextTick()
   t.is(globalState.count, 1)
-  t.is(vm.count, 1)
+  t.is(vm.globalState.count, 1)
   t.is(watchLog.count, 1)
 
   vm.$destroy()
   globalState.count++
   await vm.$nextTick()
   t.is(globalState.count, 2)
-  t.is(vm.count, 2)
+  t.is(vm.globalState.count, 2)
   t.is(watchLog.count, 1)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('immediate = true', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    immediate: true,
+    watch () {
+      return this.globalState.count
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 1)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 2)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 2)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('deep = false', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    immediate: true,
+    deep: false,
+    watch () {
+      return this.globalState
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 1)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 1)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 1)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('immediate = true', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    immediate: true,
+    watch () {
+      return this.globalState.count
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 1)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 2)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 2)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('deep = true', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    immediate: true,
+    deep: true,
+    watch () {
+      return this.globalState
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 1)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 2)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 2)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('value = 0', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    value: 0,
+    watch () {
+      return this.globalState.count
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 0)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 1)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 1)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('value = -1', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    value: -1,
+    watch () {
+      return this.globalState.count
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 1)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 2)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 2)
+  t.is(wc.unwatch, null)
+  t.is(wc.vm, null)
+})
+
+ava('value = -1 && immediate = true', async t => {
+  const watchLog = {
+    count: 0
+  }
+  const watchOptions = {
+    value: 0,
+    immediate: true,
+    watch () {
+      return this.globalState.count
+    },
+    handler () {
+      watchLog.count++
+    }
+  }
+  const wc = new WatchComponent(watchOptions)
+  const globalState = {
+    count: 0
+  }
+  const vm = new Vue({
+    watchComponents: [wc],
+    data () {
+      return { globalState }
+    }
+  })
+  t.is(watchLog.count, 1)
+  t.is(globalState.count, 0)
+  t.is(vm.globalState.count, 0)
+  t.is(typeof wc.unwatch, 'function')
+  t.is(wc.vm, vm)
+
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 1)
+  t.is(vm.globalState.count, 1)
+  t.is(watchLog.count, 2)
+
+  vm.$destroy()
+  globalState.count++
+  await vm.$nextTick()
+  t.is(globalState.count, 2)
+  t.is(vm.globalState.count, 2)
+  t.is(watchLog.count, 2)
   t.is(wc.unwatch, null)
   t.is(wc.vm, null)
 })
